6d28e576fd151a70da4612eecdf8a821ff52d708
Iliya-creator <78891186+Iliya-creator@users.noreply.github.com>
2022-08-30 10:17:09 -0700

    ADD: corrected Exercise 1 of Class 4

--- Class 4 (25 August)/Building Real Things (Class 4)/Excercise1/main.js
+++ Class 4 (25 August)/Building Real Things (Class 4)/Excercise1/main.js
@@ -1,3 +1,9 @@
 function changeBackgroundAndTextColors(){
-    document.getElementById("containingAll").style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    let color1 = "#" + Math.floor(Math.random()*16777215).toString(16);
+    let color2 = "#" + Math.floor(Math.random()*16777215).toString(16);
+    document.querySelector("*").style.color = color1;
+    document.querySelector("*").style.backgroundColor = color2;
+    document.querySelector("button").style.color = color1;
+    document.querySelector("button").style.backgroundColor = color2;
+    
 }
