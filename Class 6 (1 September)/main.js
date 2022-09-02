832c9f2e7042805441d3dfb47e09ddf2f3b3f078
Iliya-creator <78891186+Iliya-creator@users.noreply.github.com>
2022-09-01 19:05:34 -0700

    CREATE: Class 6 warmup

---
+++ Class 6 (1 September)/main.js
@@ -0,0 +1,9 @@
+// Create a function that will take one number parameter and count to that number by outputting zero to that number.
+
+function countTo(num){
+    for (let i = 0; i <= num; i++){
+        console.log(i);
+    }
+}
+
+countTo(1234);
